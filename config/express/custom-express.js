3abf8cd70dc68fd49cdd9d792793419921a5398e
Yuri Ramos <yuriramoscc@gmail.com>
2020-10-06 17:17:06 -0300

    menuzinho feito, home feito, tudo pronto para receber a lógica

--- config/express/custom-express.js
+++ config/express/custom-express.js
@@ -2,7 +2,7 @@ require('marko/node-require').install();
 require('marko/express');
 const express = require('express');
 const app = express();
-app.use('/estatico', express.static('src/app/public'));
+app.use('/estatico', express.static('./app/public'));
 
 const rotas = require('../../app/rotas/rotas');
 rotas(app);
